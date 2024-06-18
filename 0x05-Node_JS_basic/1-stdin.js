ad27fbaeb4d4c4a4836ff6676c9f08467e83f887
medazza <midazza27@gmail.com>
2024-06-18 12:42:08 +0100

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("readable", () => {
+  const inputName = process.stdin.read();
+  if (inputName !== null) {
+    process.stdout.write(`Your name is: ${inputName}`);
+  }
+});
+
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
