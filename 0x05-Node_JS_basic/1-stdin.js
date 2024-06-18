973eef999662f3608cd7a313ed4883294bb75b1c
medazza <midazza27@gmail.com>
2024-06-18 18:13:16 +0000

    fix task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const inputName = process.stdin.read();
   if (inputName !== null) {
     process.stdout.write(`Your name is: ${inputName}`);
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
