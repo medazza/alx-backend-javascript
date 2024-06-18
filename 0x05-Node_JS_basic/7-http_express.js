5cac347678db263739f9f15db97eb4702c14ec40
medazza <midazza27@gmail.com>
2024-06-18 17:14:51 +0100

    task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const fs = require("fs").promises;
+
+const PORT = 1245;
+const app = express();
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : "";
+
+function countStudents(dataPath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dataPath, "utf8")
+      .then((data) => {
+        const lines = data.split("\n");
+        const hashtable = {};
+        let students = -1;
+        let result = "";
+        for (const line of lines) {
+          if (line.trim() !== "") {
+            const studentRecord = line.split(",");
+            const field = studentRecord[studentRecord.length - 1];
+            const firstname = studentRecord[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
+            }
+            students += 1;
+          }
+        }
+        result += `Number of students: ${students}\n`;
+        for (const key in hashtable) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
+            result += `Number of students in ${key}: ${
+              hashtable[key].length
+            }. List: ${hashtable[key].join(", ")}\n`;
+          }
+        }
+        resolve(result);
+      })
+      .catch(() => {
+        reject(new Error("Cannot load the database"));
+      });
+  });
+}
+
+app.get("/", (_, res) => {
+  res.send("Hello Holberton School!");
+});
+
+app.get("/students", (_, res) => {
+  countStudents(DB_FILE)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
