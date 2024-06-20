6c5660aaaf3f1f2c2ca39518238a69260ce979e9
medazza <midazza27@gmail.com>
2024-06-20 22:10:22 +0100

    task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 7865;
 
+app.use(express.json());
+
 app.get("/", (_, res) => {
   res.send("Welcome to the payment system");
 });
